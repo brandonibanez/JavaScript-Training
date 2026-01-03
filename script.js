ab083a4b0bc2b71e114235bb16557ec7cc308250
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-01-03 20:48:24 +0800

    Values and Variables

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
 console.log(1+ 2 + 3 + 45);
+
+let firstName = "Brandon";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let PI = 3.1415;
+console.log(PI);
