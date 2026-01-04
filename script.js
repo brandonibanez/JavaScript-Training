1161273218926bc96efddbf3c027b5faced01e0f
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-01-04 14:31:36 +0800

    Basic Operators

--- script.js
+++ script.js
@@ -20,3 +20,26 @@ job = "teacher";
 
 lastName = "Smith";
 console.log(lastName);
+
+const ageJohn = 30;
+const ageMark = 25;
+console.log(ageJohn, ageMark);
+const average = (ageJohn + ageMark) / 2;
+console.log(ageJohn ** 2);
+console.log(average);
+const test1 = 'test1';
+const test2 = 'test2';
+console.log(test1 + ' ' + test2);
+
+let x = 2 + 3 * 4;
+console.log(x);
+x = (2 + 3) * 4;
+console.log(x);
+x += 10;
+console.log(x);
+x++;
+console.log(x);
+x--;
+console.log(x);
+
+console.log(ageJohn > ageMark);
