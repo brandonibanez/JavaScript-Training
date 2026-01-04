730427ed9d0b5559f03d501ca325b2e60274e6a4
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-01-04 19:58:25 +0800

    Strings & Template Literals

--- script.js
+++ script.js
@@ -1,51 +1,68 @@
-let js = "amazing";
-console.log(1 + 2 + 3 + 45);
+// let js = "amazing";
+// console.log(1 + 2 + 3 + 45);
 
-let firstName = "Brandon";
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// let firstName = "Brandon";
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
 
-let PI = 3.1415;
-console.log(PI);
+// let PI = 3.1415;
+// console.log(PI);
 
-let age = 30;
-age = 31;
+// let age = 30;
+// age = 31;
 
-const bYear = 1999;
-// bYear = 1990;
+// const bYear = 1999;
+// // bYear = 1990;
 
-var job = "programmer";
-job = "teacher";
+// var job = "programmer";
+// job = "teacher";
 
-lastName = "Smith";
-console.log(lastName);
+// lastName = "Smith";
+// console.log(lastName);
 
-const ageJohn = 30;
-const ageMark = 25;
-console.log(ageJohn, ageMark);
-const average = (ageJohn + ageMark) / 2;
-console.log(ageJohn ** 2);
-console.log(average);
-const test1 = "test1";
-const test2 = "test2";
-console.log(test1 + " " + test2);
+// const ageJohn = 30;
+// const ageMark = 25;
+// console.log(ageJohn, ageMark);
+// const average = (ageJohn + ageMark) / 2;
+// console.log(ageJohn ** 2);
+// console.log(average);
+// const test1 = "test1";
+// const test2 = "test2";
+// console.log(test1 + " " + test2);
 
-let x = 2 + 3 * 4;
-console.log(x);
-x = (2 + 3) * 4;
-console.log(x);
-x += 10;
-console.log(x);
-x++;
-console.log(x);
-x--;
-console.log(x);
+// let x = 2 + 3 * 4;
+// console.log(x);
+// x = (2 + 3) * 4;
+// console.log(x);
+// x += 10;
+// console.log(x);
+// x++;
+// console.log(x);
+// x--;
+// console.log(x);
 
-console.log(ageJohn > ageMark);
+// console.log(ageJohn > ageMark);
 
-console.log(25 - 10 - 5);
+// console.log(25 - 10 - 5);
 
-let x1, y;
-x1 = y = 25 - 10 - 5;
-console.log(x1, y);
+// let x1, y;
+// x1 = y = 25 - 10 - 5;
+// console.log(x1, y);
+
+const firstName = "Brandon";
+const job = "developer";
+const birthYear = 1999;
+
+const brandon =
+  "I'm " + firstName + ", a " + (2026 - birthYear) + " year old " + job + "!";
+console.log(brandon);
+
+const brandonNew = `I'm ${firstName}, a ${2026 - birthYear} year old ${job}!`;
+console.log(brandonNew);
+
+console.log(`Just a regular string...`);
+console.log(`String with
+multiple
+lines`);
+console.log("String with\nmultiple\nlines");
