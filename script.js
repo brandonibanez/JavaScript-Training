c85ffb5d19b7f72b134e42fed47dd61d065c8c0b
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-01-05 17:44:02 +0800

    Ternary Operator

--- script.js
+++ script.js
@@ -159,45 +159,62 @@
 //   console.log("Someone else should drive...");
 // }
 
-const day = "monday";
+// const day = "monday";
 
-const days = [
-  "monday",
-  "tuesday",
-  "wednesday",
-  "thursday",
-  "friday",
-  "saturday",
-  "sunday",
-];
+// const days = [
+//   "monday",
+//   "tuesday",
+//   "wednesday",
+//   "thursday",
+//   "friday",
+//   "saturday",
+//   "sunday",
+// ];
 
-const randomDay = days[Math.floor(Math.random() * days.length)];
+// const randomDay = days[Math.floor(Math.random() * days.length)];
 
-console.log(randomDay);
+// console.log(randomDay);
 
-switch (randomDay) {
-  case "monday":
-    console.log("Plan course structure");
-    console.log("Go to coding meetup");
-    break;
-  case "tuesday":
-    console.log("Prepare theory videos");
-    break;
-  case "wednesday":
-    console.log("Write code examples");
-    break;
-  case "thursday":
-    console.log("Record videos");
-    break;
-  case "friday":
-    console.log("Film videos");
-    break;
-  case "saturday":
-    console.log("Enjoy the weekend :D");
-    break;
-  case "sunday":
-    console.log("Prepare for the week ahead");
-    break;
-  default:
-    console.log("Not a valid day!");
+// switch (randomDay) {
+//   case "monday":
+//     console.log("Plan course structure");
+//     console.log("Go to coding meetup");
+//     break;
+//   case "tuesday":
+//     console.log("Prepare theory videos");
+//     break;
+//   case "wednesday":
+//     console.log("Write code examples");
+//     break;
+//   case "thursday":
+//     console.log("Record videos");
+//     break;
+//   case "friday":
+//     console.log("Film videos");
+//     break;
+//   case "saturday":
+//     console.log("Enjoy the weekend :D");
+//     break;
+//   case "sunday":
+//     console.log("Prepare for the week ahead");
+//     break;
+//   default:
+//     console.log("Not a valid day!");
+// }
+
+const age = 23;
+age >= 18
+  ? console.log("I like to drink wine 🍷")
+  : console.log("I like to drink water 💧");
+
+const drink = age >= 18 ? "wine 🍷" : "water 💧";
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+  drink2 = "wine 🍷";
+} else {
+  drink2 = "water 💧";
 }
+console.log(drink2);
+console.log(`I like to drink ${age >= 18 ? "wine 🍷" : "water 💧"}`);
