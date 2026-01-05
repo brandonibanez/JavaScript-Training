b0944db8b07339d77c731ba6ac70db6c501ffe9d
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-01-05 16:34:50 +0800

    Truthy & Falsy Values

--- script.js
+++ script.js
@@ -84,18 +84,38 @@
 // }
 // console.log(century);
 
-const inputYear = "1999";
-console.log(Number(inputYear) + 18);
-console.log(inputYear + 18);
-console.log(Number("test"));
-console.log(typeof NaN);
-console.log(String(23), 23);
+// const inputYear = "1999";
+// console.log(Number(inputYear) + 18);
+// console.log(inputYear + 18);
+// console.log(Number("test"));
+// console.log(typeof NaN);
+// console.log(String(23), 23);
 
-console.log("I am " + 23 + " years old");
-console.log("23" - "10" - 3);
-console.log("23" + "10" + 3);
-console.log("23" * "2");
-console.log("23" / "2");
-let n = "1" + 1;
-n = n - 1;
-console.log(n);
+// console.log("I am " + 23 + " years old");
+// console.log("23" - "10" - 3);
+// console.log("23" + "10" + 3);
+// console.log("23" * "2");
+// console.log("23" / "2");
+// let n = "1" + 1;
+// n = n - 1;
+// console.log(n);
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("Brandon"));
+console.log(Boolean({}));
+console.log(Boolean(""));
+
+const money = 100;
+if (money) {
+  console.log("Don't spend it all ;)");
+} else {
+  console.log("You should get a job!");
+}
+
+let height = 0;
+if (height) {
+  console.log("YAY! Height is defined");
+} else {
+  console.log("Height is UNDEFINED");
+}
