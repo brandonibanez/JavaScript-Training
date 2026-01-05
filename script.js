ce85643f379f73a764bcb74fe1ba6f16445837ae
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-01-05 16:43:50 +0800

    Equality Operators: == vs ===

--- script.js
+++ script.js
@@ -100,22 +100,37 @@
 // n = n - 1;
 // console.log(n);
 
-console.log(Boolean(0));
-console.log(Boolean(undefined));
-console.log(Boolean("Brandon"));
-console.log(Boolean({}));
-console.log(Boolean(""));
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean("Brandon"));
+// console.log(Boolean({}));
+// console.log(Boolean(""));
 
-const money = 100;
-if (money) {
-  console.log("Don't spend it all ;)");
+// const money = 100;
+// if (money) {
+//   console.log("Don't spend it all ;)");
+// } else {
+//   console.log("You should get a job!");
+// }
+
+// let height = 0;
+// if (height) {
+//   console.log("YAY! Height is defined");
+// } else {
+//   console.log("Height is UNDEFINED");
+// }
+
+const age = "18";
+if (age === 18) console.log("You just became an adult :D (strict)");
+if (age == 18) console.log("You just became an adult :D (loose)");
+
+const favorite = Number(prompt("What's your favorite number?"));
+if (favorite === 23) {
+  console.log("Cool! 23 is an amazing number!");
+} else if (favorite === 7) {
+  console.log("7 is also a cool number");
 } else {
-  console.log("You should get a job!");
+  console.log("Number is not 23 or 7");
 }
 
-let height = 0;
-if (height) {
-  console.log("YAY! Height is defined");
-} else {
-  console.log("Height is UNDEFINED");
-}
+if (favorite !== 23) console.log("Why not 23?");
