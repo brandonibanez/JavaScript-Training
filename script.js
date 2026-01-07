253835c3add8b419e2779bce61afc2ad85bf1213
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-01-07 18:49:39 +0800

    Functions

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ if (hasDriversLicense) console.log("I can drive :D");
 
 // const interface = "Audio";
 // const private = 534;
+
+function logger() {
+  console.log("My name is Brandon");
+}
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
