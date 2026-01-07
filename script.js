c2d0c69b6711f0ff328112b1a44b144207a1051b
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-01-07 18:55:51 +0800

    Function Declarations vs Expressions

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
 "use strict";
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log("I can drive :D");
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log("I can drive :D");
 
 // const interface = "Audio";
 // const private = 534;
 
-function logger() {
-  console.log("My name is Brandon");
-}
-logger();
-logger();
-logger();
+// function logger() {
+//   console.log("My name is Brandon");
+// }
+// logger();
+// logger();
+// logger();
 
-function fruitProcessor(apples, oranges) {
-  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-  return juice;
-}
+// function fruitProcessor(apples, oranges) {
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//   return juice;
+// }
 
-const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice);
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+
+//Function Declaration
+function calcAge1(birthYear) {
+  return 2026 - birthYear;
+}
+const age1 = calcAge1(1999);
+console.log(age1);
+
+//Function Expression
+const calcAge2 = function (birthYear) {
+  return 2026 - birthYear;
+};
+const age2 = calcAge2(1999);
+console.log(age2);
