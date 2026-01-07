f0b5dbbc1ee74109555e930fb9f156c3015e3cfa
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-01-07 19:47:15 +0800

    Functions Calling Other Functions

--- script.js
+++ script.js
@@ -39,14 +39,26 @@
 // console.log(age2);
 
 //Arrow Function
-const calcAge3 = (birthYear) => 2026 - birthYear;
-const age3 = calcAge3(1999);
-console.log(age3);
+// const calcAge3 = (birthYear) => 2026 - birthYear;
+// const age3 = calcAge3(1999);
+// console.log(age3);
 
-const yearsUntilRetirement = (birthYear, firstName) => {
-  const age = 2026 - birthYear;
-  const retirement = 65 - age;
-  return `${firstName} retires in ${retirement} years.`;
-};
-console.log(yearsUntilRetirement(1999, "Brandon"));
-console.log(yearsUntilRetirement(1980, "Bob"));
+// const yearsUntilRetirement = (birthYear, firstName) => {
+//   const age = 2026 - birthYear;
+//   const retirement = 65 - age;
+//   return `${firstName} retires in ${retirement} years.`;
+// };
+// console.log(yearsUntilRetirement(1999, "Brandon"));
+// console.log(yearsUntilRetirement(1980, "Bob"));
+
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+  const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+  return juice;
+}
+console.log(fruitProcessor(2, 3));
