33b683aac9358adae2e0d0d005c337cb3571706b
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-01-09 14:58:15 +0800

    Introduction to Arrays

--- script.js
+++ script.js
@@ -51,14 +51,53 @@
 // console.log(yearsUntilRetirement(1999, "Brandon"));
 // console.log(yearsUntilRetirement(1980, "Bob"));
 
-function cutFruitPieces(fruit) {
-  return fruit * 4;
-}
+// function cutFruitPieces(fruit) {
+//   return fruit * 4;
+// }
 
-function fruitProcessor(apples, oranges) {
-  const applePieces = cutFruitPieces(apples);
-  const orangePieces = cutFruitPieces(oranges);
-  const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
-  return juice;
-}
-console.log(fruitProcessor(2, 3));
+// function fruitProcessor(apples, oranges) {
+//   const applePieces = cutFruitPieces(apples);
+//   const orangePieces = cutFruitPieces(oranges);
+//   const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+//   return juice;
+// }
+// console.log(fruitProcessor(2, 3));
+
+// const calcAverage = (num1, num2, num3) => (num1 + num2 + num3) / 3;
+
+// console.log(calcAverage(1, 2, 3));
+
+const friends = ["Michael", "Steven", "Peter"];
+console.log(friends);
+
+const years = new Array(1999, 1984, 2008, 2020);
+console.log(years);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = "Jay";
+console.log(friends);
+
+const firstName = "Brandon";
+const brandon = [firstName, "Smith", 2026 - 1999, "teacher", friends];
+console.log(brandon);
+console.log(brandon[4]);
+
+const calcAge = function (birthYear) {
+  return 2026 - birthYear;
+};
+const years2 = [1999, 1984, 2008, 2020];
+const age1 = calcAge(years2[0]);
+const age2 = calcAge(years2[1]);
+const age3 = calcAge(years2[years2.length - 1]);
+console.log(age1, age2, age3);
+const ages = [
+  calcAge(years2[0]),
+  calcAge(years2[1]),
+  calcAge(years2[years2.length - 1]),
+];
+console.log(ages);
