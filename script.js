3e3ef5f3b5eefb9258b540662bc45d6bf49a77f4
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-01-09 19:24:15 +0800

    Basic For Loop

--- script.js
+++ script.js
@@ -167,24 +167,28 @@
 // brandon["twitter"] = "@brandonsmith";
 // console.log(brandon);
 
-const brandon = {
-  firstName: "Brandon",
-  lastName: "Smith",
-  birthYear: 1999,
-  job: "teacher",
-  friends: ["Michael", "Peter", "Steven"],
-  hasDriversLicense: true,
-  calcAge: function () {
-    this.age = 2026 - this.birthYear;
-    return this.age;
-  },
-  getSummary: function () {
-    return `${this.firstName} is a ${this.calcAge()}-year-old ${
-      this.job
-    }, and he has ${this.hasDriversLicense ? "a" : "no"} driver's license.`;
-  },
-};
-console.log(brandon.calcAge());
-console.log(brandon.age);
-console.log(brandon.getSummary());
-console.log(brandon);
+// const brandon = {
+//   firstName: "Brandon",
+//   lastName: "Smith",
+//   birthYear: 1999,
+//   job: "teacher",
+//   friends: ["Michael", "Peter", "Steven"],
+//   hasDriversLicense: true,
+//   calcAge: function () {
+//     this.age = 2026 - this.birthYear;
+//     return this.age;
+//   },
+//   getSummary: function () {
+//     return `${this.firstName} is a ${this.calcAge()}-year-old ${
+//       this.job
+//     }, and he has ${this.hasDriversLicense ? "a" : "no"} driver's license.`;
+//   },
+// };
+// console.log(brandon.calcAge());
+// console.log(brandon.age);
+// console.log(brandon.getSummary());
+// console.log(brandon);
+
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repetition ${rep} 🏋️‍♂️`);
+}
