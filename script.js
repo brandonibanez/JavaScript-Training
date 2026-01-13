c2684eca475130c7d9cb382d917cdc3f0ca62463
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-01-13 17:21:18 +0800

    Implementing Highscores

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ let number = Math.trunc(Math.random() * 20) + 1;
 console.log(number);
 
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -24,6 +25,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = number;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
   } else if (guess > number) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too high!';
