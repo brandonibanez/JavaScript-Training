94854bafeea87d0ca29db793a4f2ec12c023b06e
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-01-13 17:30:19 +0800

    Refactoring: The DRY Principle

--- script.js
+++ script.js
@@ -30,19 +30,10 @@ document.querySelector('.check').addEventListener('click', function () {
       highscore = score;
       document.querySelector('.highscore').textContent = highscore;
     }
-  } else if (guess > number) {
+  } else if (guess !== number) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Too high!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'You lost the game!';
-      document.querySelector('.score').textContent = 0;
-      score--;
-    }
-  } else if (guess < number) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'Too low!';
+      document.querySelector('.message').textContent =
+        guess > number ? 'Too high!' : 'Too low!';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
