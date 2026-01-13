59c9392be3f08bc64fa8341d3e9459665012120c
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-01-13 16:22:08 +0800

    Handling Click Events

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Hello JavaScript!';
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Hello JavaScript!';
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
 
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+  } else if (guess === 23) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+    document.querySelector('.number').textContent = 23;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+  } else if (guess > 23) {
+    document.querySelector('.message').textContent = 'Too high!';
+  } else if (guess < 23) {
+    document.querySelector('.message').textContent = 'Too low!';
+  }
+});
