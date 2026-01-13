de7e6a767a780057c84f0a691f359c20de687ecf
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-01-13 16:36:57 +0800

    Implementing the Game Logic

--- script.js
+++ script.js
@@ -8,6 +8,10 @@
 
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
+const number = Math.trunc(Math.random() * 20) + 1;
+console.log(number);
+
+let score = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -15,14 +19,28 @@ document.querySelector('.check').addEventListener('click', function () {
 
   if (!guess) {
     document.querySelector('.message').textContent = 'No number!';
-  } else if (guess === 23) {
+  } else if (guess === number) {
     document.querySelector('.message').textContent = 'Correct Number!';
-    document.querySelector('.number').textContent = 23;
+    document.querySelector('.number').textContent = number;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
-  } else if (guess > 23) {
-    document.querySelector('.message').textContent = 'Too high!';
-  } else if (guess < 23) {
-    document.querySelector('.message').textContent = 'Too low!';
+  } else if (guess > number) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < number) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
