eb6421a5e239af8682b12ac113f4b326c532a571
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-01-14 20:45:22 +0800

    Hoisting and TDZ

--- script.js
+++ script.js
@@ -1,36 +1,75 @@
 'use strict';
 
-function calcAge(birthYear) {
-  const age = 2026 - birthYear;
+// function calcAge(birthYear) {
+//   const age = 2026 - birthYear;
 
-  function printAge() {
-    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
-    console.log(output);
+//   function printAge() {
+//     let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+//     console.log(output);
 
-    if (birthYear >= 1981 && birthYear <= 1996) {
-      var millennial = true;
-      // Creating NEW variable with same name as outer scope's variable
-      const firstName = 'Ivan';
-      // Reassigning outer scope's variable
-      output = 'NEW OUTPUT!';
-      const str = `Oh, and you're a millennial, ${firstName}`;
-      console.log(str);
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       var millennial = true;
+//       // Creating NEW variable with same name as outer scope's variable
+//       const firstName = 'Ivan';
+//       // Reassigning outer scope's variable
+//       output = 'NEW OUTPUT!';
+//       const str = `Oh, and you're a millennial, ${firstName}`;
+//       console.log(str);
 
-      function add(a, b) {
-        return a + b;
-      }
-      console.log(add(2, 3));
-    }
-    // console.log(str); // Error: str is not defined
-    console.log(millennial); // Works due to var scoping
-    // add(2, 3); // Error: add is not defined
-    console.log(output);
-  }
-  printAge();
+//       function add(a, b) {
+//         return a + b;
+//       }
+//       console.log(add(2, 3));
+//     }
+//     // console.log(str); // Error: str is not defined
+//     console.log(millennial); // Works due to var scoping
+//     // add(2, 3); // Error: add is not defined
+//     console.log(output);
+//   }
+//   printAge();
 
-  return age;
+//   return age;
+// }
+
+// const firstName = 'Brandon';
+// const myAge = calcAge(1996);
+// console.log(myAge);
+
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Hello';
+let job = 'Developer';
+const year = 2026;
+
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
 }
 
-const firstName = 'Brandon';
-const myAge = calcAge(1996);
-console.log(myAge);
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
