a7e458793c86c65c55c0cad43b33cd6907f45d46
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-01-16 17:54:15 +0800

    Logical Assignment Operators

--- script.js
+++ script.js
@@ -72,12 +72,30 @@ const restaurant = {
   },
 };
 
-restaurant.numGuests = 0;
-const guests2 = restaurant.numGuests || 10;
-console.log(guests2);
+const rest1 = { name: 'Capri', numGuests: 0 };
+const rest2 = { name: 'La Piazza', owner: 'Giovanni Rossi' };
 
-const guestCorrect = restaurant.numGuests ?? 10;
-console.log(guestCorrect);
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+
+console.log(rest1);
+console.log(rest2);
+
+// restaurant.numGuests = 0;
+// const guests2 = restaurant.numGuests || 10;
+// console.log(guests2);
+
+// const guestCorrect = restaurant.numGuests ?? 10;
+// console.log(guestCorrect);
 
 // console.log(3 || 'Brandon');
 // console.log('' || 'Brandon');
