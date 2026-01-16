caab1e76e3c2b543ea947d455c881c39a1c41c03
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-01-16 22:21:49 +0800

    Optional Chaining

--- script.js
+++ script.js
@@ -68,14 +68,33 @@ const restaurant = {
   },
 };
 
-const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
-
-// for (const item of menu) console.log(item);
-for (const item of menu) console.log(item);
-
-for (const [i, el] of menu.entries()) {
-  console.log(`${i + 1}: ${el}`);
+if (restaurant.openingHours && restaurant.openingHours.mon) {
+  console.log(restaurant.openingHours.mon.open);
 }
+
+console.log(restaurant.openingHours?.mon?.open);
+
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+
+const users = [{ name: 'Brandon', email: 'brandon@example.com' }];
+
+console.log(users[0]?.name ?? 'User array empty');
+
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+// // for (const item of menu) console.log(item);
+// for (const item of menu) console.log(item);
+
+// for (const [i, el] of menu.entries()) {
+//   console.log(`${i + 1}: ${el}`);
+// }
 
 // const rest1 = { name: 'Capri', numGuests: 0 };
 // const rest2 = { name: 'La Piazza', owner: 'Giovanni Rossi' };
