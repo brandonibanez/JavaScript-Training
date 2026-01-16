893fb87b31192371bd7020a42d1366fe8db0edeb
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-01-16 22:28:29 +0800

    Looping Objects: Keys, Values, and Entries

--- script.js
+++ script.js
@@ -68,24 +68,43 @@ const restaurant = {
   },
 };
 
-if (restaurant.openingHours && restaurant.openingHours.mon) {
-  console.log(restaurant.openingHours.mon.open);
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+for (const day of properties) {
+  const open = openingHours[day].open;
+  const close = openingHours[day].close;
+  console.log(`On ${day}, we open at ${open} and close at ${close}`);
 }
 
-console.log(restaurant.openingHours?.mon?.open);
+const values = Object.values(openingHours);
+console.log(values);
 
-const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
-for (const day of days) {
-  const open = restaurant.openingHours[day]?.open ?? 'closed';
-  console.log(`On ${day}, we open at ${open}`);
+const entries = Object.entries(openingHours);
+console.log(entries);
+
+for (const [day, { open, close }] of entries) {
+  console.log(`On ${day}, we open at ${open} and close at ${close}`);
 }
 
-console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
-console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+// if (restaurant.openingHours && restaurant.openingHours.mon) {
+//   console.log(restaurant.openingHours.mon.open);
+// }
 
-const users = [{ name: 'Brandon', email: 'brandon@example.com' }];
+// console.log(restaurant.openingHours?.mon?.open);
 
-console.log(users[0]?.name ?? 'User array empty');
+// const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+// for (const day of days) {
+//   const open = restaurant.openingHours[day]?.open ?? 'closed';
+//   console.log(`On ${day}, we open at ${open}`);
+// }
+
+// console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+// console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+
+// const users = [{ name: 'Brandon', email: 'brandon@example.com' }];
+
+// console.log(users[0]?.name ?? 'User array empty');
 
 // const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
