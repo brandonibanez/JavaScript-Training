8fc9707fa0fb63098c1a58035c7b7a434644115e
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-01-18 21:13:30 +0800

    Functions Returning Functions

--- script.js
+++ script.js
@@ -56,31 +56,50 @@
 // newPassport(brandon);
 // checkIn(flight, brandon);
 
-const oneWord = function (str) {
-  return str.replace(/ /g, '').toLowerCase();
+// const oneWord = function (str) {
+//   return str.replace(/ /g, '').toLowerCase();
+// };
+
+// const upperFirstWord = function (str) {
+//   const [first, ...others] = str.split(' ');
+//   return [first.toUpperCase(), ...others].join(' ');
+// };
+
+// // Higher-order function
+// const transformer = function (str, fn) {
+//   console.log(`Original string: ${str}`);
+//   console.log(`Transformed string: ${fn(str)}`);
+
+//   console.log(`Transformed by: ${fn.name}`);
+// };
+
+// transformer('JavaScript is the best!', upperFirstWord);
+// transformer('JavaScript is the best!', oneWord);
+
+// // JS uses callbacks all the time
+// const high5 = function () {
+//   console.log('👋');
+// };
+
+// document.body.addEventListener('click', high5);
+
+// ['Brandon', 'Sarah', 'Adam'].forEach(high5);
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
 };
 
-const upperFirstWord = function (str) {
-  const [first, ...others] = str.split(' ');
-  return [first.toUpperCase(), ...others].join(' ');
+const greeterHey = greet('Hey');
+greeterHey('Brandon');
+greeterHey('Steven');
+
+greet('Hello')('Michael');
+
+// Challenge
+const greetArrow = greeting => name => {
+  console.log(`${greeting} ${name}`);
 };
 
-// Higher-order function
-const transformer = function (str, fn) {
-  console.log(`Original string: ${str}`);
-  console.log(`Transformed string: ${fn(str)}`);
-
-  console.log(`Transformed by: ${fn.name}`);
-};
-
-transformer('JavaScript is the best!', upperFirstWord);
-transformer('JavaScript is the best!', oneWord);
-
-// JS uses callbacks all the time
-const high5 = function () {
-  console.log('👋');
-};
-
-document.body.addEventListener('click', high5);
-
-['Brandon', 'Sarah', 'Adam'].forEach(high5);
+greetArrow('Hi')('Anna');
