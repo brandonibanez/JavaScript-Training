3f25c5ad82721a5ebc5320c28d02950aa528ea7d
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-01-18 13:39:19 +0800

    Function Default Parameters

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const booking = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers,
+) {
+  // ES5 way of setting default parameters
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
+
+  const bookingDetails = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+
+  console.log(bookingDetails);
+  booking.push(bookingDetails);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 5);
+createBooking('LH123', undefined, 1000);
+
+console.log(booking);
