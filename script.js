6965b3c6377980280e8470173316f1743f9c5f76
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-01-21 18:23:26 +0800

    Find Method

--- script.js
+++ script.js
@@ -128,11 +128,18 @@ calcDisplaySummary(account1.movements);
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-const totalDeposits = movements
-  .filter(mov => mov > 0)
-  .map(mov => mov * 1.1)
-  .reduce((acc, mov) => acc + mov, 0);
-console.log(totalDeposits);
+movements.find(mov => mov < 0);
+
+console.log(movements.find(mov => mov < 0));
+
+const account = accounts.find(acc => acc.owner === 'Jessica Davis');
+console.log(account);
+
+// const totalDeposits = movements
+//   .filter(mov => mov > 0)
+//   .map(mov => mov * 1.1)
+//   .reduce((acc, mov) => acc + mov, 0);
+// console.log(totalDeposits);
 // console.log(movements.reduce((acc, mov) => acc + mov, 0));
 
 // let balance = 0;
