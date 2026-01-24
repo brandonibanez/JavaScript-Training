7b50a68fe131fe75c7dc1653fb4e3d0ab2438703
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-01-24 15:54:07 +0800

    Styles, Attributes, and Classes

--- script.js
+++ script.js
@@ -66,3 +66,42 @@ document
   .addEventListener('click', function () {
     message.remove();
   });
+
+// Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+// document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.className);
+
+logo.alt = 'Beautiful minimalist logo';
+
+console.log(logo.getAttribute('designer'));
+console.log(logo.src);
+console.log(logo.getAttribute('src'));
+
+logo.setAttribute('company', 'Bankist');
+
+console.log(logo.getAttribute('company'));
+
+// Data attributes
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('c', 'j');
+logo.classList.remove('c', 'j');
+logo.classList.toggle('c');
+logo.classList.contains('c'); // not includes
