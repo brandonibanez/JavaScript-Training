b629f50b617d203d8d7c70d6f4496d3c3e4450bd
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-01-24 16:29:06 +0800

    Implementing Smooth Scrolling

--- script.js
+++ script.js
@@ -30,78 +30,110 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-console.log(document.documentElement);
-console.log(document.head);
-console.log(document.body);
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
-const header = document.querySelector('.header');
-const allSections = document.querySelectorAll('.section');
-console.log(allSections);
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
 
-document.getElementById('section--1');
+  console.log(e.target.getBoundingClientRect());
 
-const allButtons = document.getElementsByTagName('button');
-console.log(allButtons);
+  console.log('Current scroll (X/Y): ', window.pageXOffset, window.pageYOffset);
 
-console.log(document.getElementsByClassName('btn'));
+  console.log(
+    'height/width viewport: ',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth,
+  );
 
-document.createElement('div');
-const message = document.createElement('div');
-message.classList.add('cookie-message');
-// message.textContent =
-//   'We use cookies for improved functionality and analytics.';
-message.innerHTML =
-  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
 
-// header.prepend(message);
-header.append(message);
-// header.append(message.cloneNode(true));
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
 
-// header.before(message);
-// header.after(message);
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
 
-// Delete cookie message
-document
-  .querySelector('.btn--close-cookie')
-  .addEventListener('click', function () {
-    message.remove();
-  });
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
 
-// Styles
-message.style.backgroundColor = '#37383d';
-message.style.width = '120%';
+// const header = document.querySelector('.header');
+// const allSections = document.querySelectorAll('.section');
+// console.log(allSections);
 
-console.log(message.style.color);
-console.log(message.style.backgroundColor);
+// document.getElementById('section--1');
 
-console.log(getComputedStyle(message).color);
-console.log(getComputedStyle(message).height);
+// const allButtons = document.getElementsByTagName('button');
+// console.log(allButtons);
 
-message.style.height =
-  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+// console.log(document.getElementsByClassName('btn'));
 
-// document.documentElement.style.setProperty('--color-primary', 'orangered');
+// document.createElement('div');
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
+// // message.textContent =
+// //   'We use cookies for improved functionality and analytics.';
+// message.innerHTML =
+//   'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
 
-// Attributes
-const logo = document.querySelector('.nav__logo');
-console.log(logo.alt);
-console.log(logo.className);
+// // header.prepend(message);
+// header.append(message);
+// // header.append(message.cloneNode(true));
 
-logo.alt = 'Beautiful minimalist logo';
+// // header.before(message);
+// // header.after(message);
 
-console.log(logo.getAttribute('designer'));
-console.log(logo.src);
-console.log(logo.getAttribute('src'));
+// // Delete cookie message
+// document
+//   .querySelector('.btn--close-cookie')
+//   .addEventListener('click', function () {
+//     message.remove();
+//   });
 
-logo.setAttribute('company', 'Bankist');
+// // Styles
+// message.style.backgroundColor = '#37383d';
+// message.style.width = '120%';
 
-console.log(logo.getAttribute('company'));
+// console.log(message.style.color);
+// console.log(message.style.backgroundColor);
 
-// Data attributes
-console.log(logo.dataset.versionNumber);
+// console.log(getComputedStyle(message).color);
+// console.log(getComputedStyle(message).height);
 
-// Classes
-logo.classList.add('c', 'j');
-logo.classList.remove('c', 'j');
-logo.classList.toggle('c');
-logo.classList.contains('c'); // not includes
+// message.style.height =
+//   Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+// // document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// // Attributes
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.alt);
+// console.log(logo.className);
+
+// logo.alt = 'Beautiful minimalist logo';
+
+// console.log(logo.getAttribute('designer'));
+// console.log(logo.src);
+// console.log(logo.getAttribute('src'));
+
+// logo.setAttribute('company', 'Bankist');
+
+// console.log(logo.getAttribute('company'));
+
+// // Data attributes
+// console.log(logo.dataset.versionNumber);
+
+// // Classes
+// logo.classList.add('c', 'j');
+// logo.classList.remove('c', 'j');
+// logo.classList.toggle('c');
+// logo.classList.contains('c'); // not includes
