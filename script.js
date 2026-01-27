137f4693a0c281d4b9efa70ad7f494dfbf178459
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-01-27 18:04:41 +0800

    Constructor Functions and the new Operator

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  this.calcAge = function () {
+    const currentYear = new Date().getFullYear();
+    console.log(currentYear - this.birthYear);
+  };
+};
+
+const john = new Person('John', 1990);
+console.log(john);
+
+const jane = new Person('Jane', 1995);
+console.log(jane);
+
+console.log(john instanceof Person); // true
