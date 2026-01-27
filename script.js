b9a8ec54d000d4278d887ff0bdbcfd20af0ebf07
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-01-27 18:17:25 +0800

    Prototypes

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ const Person = function (firstName, birthYear) {
   this.firstName = firstName;
   this.birthYear = birthYear;
 
-  this.calcAge = function () {
-    const currentYear = new Date().getFullYear();
-    console.log(currentYear - this.birthYear);
-  };
+  // this.calcAge = function () {
+  //   const currentYear = new Date().getFullYear();
+  //   console.log(currentYear - this.birthYear);
+  // };
 };
 
 const john = new Person('John', 1990);
@@ -17,3 +17,22 @@ const jane = new Person('Jane', 1995);
 console.log(jane);
 
 console.log(john instanceof Person); // true
+
+// Prototypes
+Person.prototype.calcAge = function () {
+  const currentYear = new Date().getFullYear();
+  console.log(currentYear - this.birthYear);
+};
+
+john.calcAge(); // 33
+jane.calcAge(); // 28
+
+console.log(john.__proto__); // Person.prototype
+console.log(john.__proto__ === Person.prototype); // true
+console.log(Person.prototype.isPrototypeOf(john)); // true
+console.log(Person.prototype.isPrototypeOf(jane)); // true
+console.log(Person.prototype.isPrototypeOf(Person)); // false
+
+// Adding properties to prototype
+Person.prototype.species = 'Homo Sapiens';
+console.log(john.species, jane.species);
