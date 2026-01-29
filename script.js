227de7a8ace583a6cf3820e00061fd789bf3ccb4
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-01-29 12:43:31 +0800

    Using Geolocation API

--- script.js
+++ script.js
@@ -10,3 +10,30 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    const { latitude, longitude } = position.coords;
+    console.log(position);
+    console.log(`https://www.google.com/maps/@${latitude},${longitude},15z`);
+  },
+  function () {
+    alert('Could not get your position');
+  },
+);
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  // Clear input fields
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      '';
+});
+
+inputType.addEventListener('change', function () {
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+});
