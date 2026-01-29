5c07759c4e6f5d5264ab82596119b4b1dc042a0a
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-01-29 12:53:05 +0800

    Displaying a Map Using Leaflet Library

--- script.js
+++ script.js
@@ -14,8 +14,20 @@ const inputElevation = document.querySelector('.form__input--elevation');
 navigator.geolocation.getCurrentPosition(
   function (position) {
     const { latitude, longitude } = position.coords;
-    console.log(position);
     console.log(`https://www.google.com/maps/@${latitude},${longitude},15z`);
+
+    var map = L.map('map').setView([latitude, longitude], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    // Handling clicks on map
+    L.marker([latitude, longitude])
+      .addTo(map)
+      .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+      .openPopup();
   },
   function () {
     alert('Could not get your position');
