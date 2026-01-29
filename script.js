fdb12a224e357c84d3426ee2520ec9b8db0b889b
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-01-29 14:51:41 +0800

    Rendering Workout Input Form

--- script.js
+++ script.js
@@ -11,33 +11,24 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapE;
+
 navigator.geolocation.getCurrentPosition(
   function (position) {
     const { latitude, longitude } = position.coords;
     console.log(`https://www.google.com/maps/@${latitude},${longitude},15z`);
 
-    const map = L.map('map').setView([latitude, longitude], 13);
+    map = L.map('map').setView([latitude, longitude], 13);
 
     L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
 
-    map.on('click', function (mapE) {
-      const { lat, lng } = mapE.latlng;
-      L.marker([lat, lng])
-        .addTo(map)
-        .bindPopup(
-          L.popup({
-            maxWidth: 250,
-            minWidth: 100,
-            autoClose: false,
-            closeOnClick: false,
-            className: 'running-popup',
-          }),
-        )
-        .setPopupContent('Workout')
-        .openPopup();
+    map.on('click', function (mapEvent) {
+      mapE = mapEvent;
+      form.classList.remove('hidden');
+      inputDistance.focus();
     });
   },
   function () {
@@ -47,6 +38,21 @@ navigator.geolocation.getCurrentPosition(
 
 form.addEventListener('submit', function (e) {
   e.preventDefault();
+
+  const { lat, lng } = mapE.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      }),
+    )
+    .setPopupContent('Workout')
+    .openPopup();
 
   // Clear input fields
   inputDistance.value =
