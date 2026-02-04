1baaf7ee2505f4229abffb3c7e7887c6a12c77fe
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-02-04 16:57:32 +0800

    Top-Level await

--- script.js
+++ script.js
@@ -5,8 +5,24 @@
 // import * as ShoppingCart from './shoppingCart.js';
 import add, { cart } from './shoppingCart.js';
 
-add('bread', 5);
+// add('bread', 5);
 
 // ShoppingCart.addToCart('apples', 5);
 // console.log(ShoppingCart.totalPrice, ShoppingCart.totalQuantity);
-console.log(cart);
+// console.log(cart);
+
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data[0]);
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = await getLastPost();
+console.log(lastPost);
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
--- shoppingCart.js
+++ shoppingCart.js
@@ -4,6 +4,10 @@ console.log('Exporting module');
 const shippinngCost = 10;
 export const cart = [];
 
+// console.log('Start fetching users');
+// await fetch('https://jsonplaceholder.typicode.com/users');
+// console.log('Finished fetching users');
+
 export const addToCart = function (product, quantity) {
   cart.push({ product, quantity });
   console.log(
