3134f13846b88d29623471e83438e3228e1a9f70
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-02-05 23:28:12 +0800

    Listening For load and hashchange Events

--- src/js/controller.js
+++ src/js/controller.js
@@ -31,9 +31,13 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+
     renderSpinner(recipeContainer);
     const res = await fetch(
-      'https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886',
+      `https://forkify-api.jonas.io/api/v2/recipes/${id}`,
     );
     const data = await res.json();
 
@@ -126,5 +130,7 @@ const showRecipe = async function () {
     console.error(`${err} 💥💥💥`);
   }
 };
-
-showRecipe();
+// showRecipe();
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
+// window.addEventListener('hashchange', showRecipe);
+// window.addEventListener('load', showRecipe);
