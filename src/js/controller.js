568f1aa4eec7e94b2a51bf96de19f41bf8f49174
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-02-06 14:51:58 +0800

    Helpers and Configuration Files

--- src/js/controller.js
+++ src/js/controller.js
@@ -6,14 +6,6 @@ import 'regenerator-runtime/runtime';
 
 const recipeContainer = document.querySelector('.recipe');
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
 // NEW API URL (instead of the one shown in the video)
 // https://forkify-api.jonas.io
 
@@ -33,7 +25,7 @@ const controlRecipes = async function () {
     //2 Rendering recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
-    console.error(`${err} 💥💥💥`);
+    console.error(err);
   }
 };
 // controlRecipes();
--- src/js/model.js
+++ src/js/model.js
@@ -1,14 +1,13 @@
+import { API_URL } from './config.js';
+import { getJSON } from './helpers.js';
+
 export const state = {
   recipe: {},
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const res = await fetch(
-      `https://forkify-api.jonas.io/api/v2/recipes/${id}`,
-    );
-    const data = await res.json();
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    const data = await getJSON(`${API_URL}${id}`);
 
     const { recipe } = data.data;
     state.recipe = {
@@ -23,6 +22,5 @@ export const loadRecipe = async function (id) {
     };
   } catch (err) {
     console.error(`${err} 💥💥💥`);
-    throw err;
   }
 };
