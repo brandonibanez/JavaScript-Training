c832e137f0a7b7f4a7c146495cb30323a6a15f19
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-02-06 18:23:23 +0800

    Completing Search Results

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,11 +1,16 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-import searchView from './views/searchView.js';
 
 const recipeContainer = document.querySelector('.recipe');
+
+if (module.hot) {
+  module.hot.accept();
+}
 
 // NEW API URL (instead of the one shown in the video)
 // https://forkify-api.jonas.io
@@ -32,6 +37,7 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
     //1 Get search query
     const query = searchView.getQuery();
     if (!query) return;
@@ -41,6 +47,7 @@ const controlSearchResults = async function () {
 
     //3 Render results
     console.log(model.state.search.results);
+    resultsView.render(model.state.search.results);
   } catch (err) {
     console.log(err);
   }
